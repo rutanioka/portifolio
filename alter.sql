32fab31ded0638691b9eeed5e26733e1dc13dc82
rutanioka <rutanioka@gmail.com>
2022-01-21 22:17:40 -0300

    Iniciado o protocolo de consulta de um arquivo escolar com banco de dados em SQL

---
+++ alter.sql
@@ -0,0 +1,36 @@
+alter_participant = """
+ALTER TABLE participant
+ADD FOREIGN KEY(client)
+REFERENCES client(client_id)
+ON DELETE SET NULL;
+"""
+
+alter_course = """
+ALTER TABLE course
+ADD FOREIGN KEY(teacher)
+REFERENCES teacher(teacher_id)
+ON DELETE SET NULL;
+"""
+
+alter_course_again = """
+ALTER TABLE course
+ADD FOREIGN KEY(client)
+REFERENCES client(client_id)
+ON DELETE SET NULL;
+"""
+
+create_takescourse_table = """
+CREATE TABLE takes_course (
+  participant_id INT,
+  course_id INT,
+  PRIMARY KEY(participant_id, course_id),
+  FOREIGN KEY(participant_id) REFERENCES participant(participant_id) ON DELETE CASCADE,
+  FOREIGN KEY(course_id) REFERENCES course(course_id) ON DELETE CASCADE
+);
+"""
+
+connection = create_db_connection("localhost", "root", pw, db)
+execute_query(connection, alter_participant)
+execute_query(connection, alter_course)
+execute_query(connection, alter_course_again)
+execute_query(connection, create_takescourse_table)
---
+++ consulta_escolar.py
@@ -0,0 +1,83 @@
+#instalar biblioteca
+pip install mysql-connector-python
+
+from multiprocessing import connection
+import pwd
+from sqlite3 import Cursor, connect
+from tkinter import _Cursor
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+# criar a conexão
+def create_server_connection (host_name, user_name, user_pasword):
+    connection = None
+    try:
+        connection = mysql.connector.connect (
+            host = host_name
+            user = user_name
+            passwd = user_pasword
+        )
+        print ("MySQL Database connection sucessful")
+    except Error as err:
+        print ("Error:'{err}'")
+    return connection
+    #TODO melhorar testes de conexão
+
+
+#criar banco de dados
+def create_database(connection,query):
+    cursor = connection.cursor():
+    try:
+        cursor.execute(query)
+        print("Database created sucessfully")
+    except Error as err:
+        print ("Error: '{err}'")
+
+
+#Conectar com o banco de dados
+def creat_db_connection(host_name, user_name, user_pasword, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name
+            user = user_name
+            passwd = user_pasword
+            database = db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print("'Error: '{err}'")
+    return connection
+
+# Realizando a leitura
+def read_query(connention, query):
+    cursor - connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as err:
+        print("Error:'{err}'")
+
+#para as consultas:
+q1 = """
+SELECT *
+FROM User;
+"""
+connection = creat_db_connection("localhost", "root", pw,db)
+results = read_query(connention, q1)
+
+for result in results:
+    print(result)
+    
+#formatando a lista de saída em um data frame 
+from_db = []
+
+for result in results:
+    result = list(result)
+    from_db.append(result)
+    
+columns = ["curso_id", "nome_curso","nome_cliente"]
+df = pd.DataFrame(from_db, columns = columns)
